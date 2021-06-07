a36b624729cb288d213e48db0b2cc1078e1e633d
mattiyee <mattiyee610@gmail.com>
2021-06-07 12:14:43 -0400

    initial commit

---
+++ SyntaxExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SyntaxExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Original Code
+            int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + "is less than nine.";
+            }
+            else
+            {
+                response = answer + "greater than or equal to nine.";
+            }
+
+            // New Code
+            var newAnswer = 4;
+            var newResponse = (answer < 9) ? newAnswer + "is less than nine." : newAnswer + "greater than or equal to nine.";
+
+        }
+    }
+}
